cae95b6c704a9ee271712105e8378850767d3968
arsamparsa777 <62879887+ArsamCodex@users.noreply.github.com>
2020-04-17 09:03:27 +0200

    Algorythme playin

---
+++ src/FootInch/App.java
@@ -0,0 +1,27 @@
+package FootInch;
+
+import java.util.Scanner;
+
+public class App {
+
+    private static int foot;
+    private static int inch;
+    private static float foot_to_meter;
+    private static float foot_to_centimeter;
+    private static float inch_to_centimeter;
+    private static float inch_to_meter;
+
+    private static void read(){
+        System.out.println("FOOT");
+        foot =( new Scanner(System.in)).nextInt();
+        System.out.println("INCH");
+        inch = new Scanner(System.in).nextInt();
+    }
+
+    private static void Calculate(){
+        
+
+    }
+
+
+}
