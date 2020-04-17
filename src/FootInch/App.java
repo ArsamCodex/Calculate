720a0c4bec2c22e4a1ec56a9ef66ae507cb00e83
ArsamCodex <62879887+ArsamCodex@users.noreply.github.com>
2020-04-17 09:17:24 +0200

    III

--- src/FootInch/App.java
+++ src/FootInch/App.java
@@ -1,8 +1,12 @@
 package FootInch;
 
+import com.sun.java.accessibility.util.EventID;
+
 import java.util.Scanner;
 
 public class App {
+    
+    // Static Method , Algorythme , playing ,Problemen Oplossen en nog bezig zij met Static methids 
 
     private static int foot;
     private static int inch;
@@ -19,9 +23,25 @@ public class App {
     }
 
     private static void Calculate(){
-        
+        foot_to_meter = 0.3048f * foot;
+        foot_to_centimeter = 100 * foot_to_meter;
+        inch_to_meter = (float)(1.0f / 12) * inch;
+        inch_to_centimeter = 100 * inch_to_meter;
 
     }
 
+    private static void write(){
+        System.out.printf("The %1$s foor is %2S$ meter" + "\r\n" ,foot , foot_to_meter);
+        System.out.printf(" The %1$s foot is %2$s Centimeter" + "\r\n"  ,foot,foot_to_centimeter);
+        System.out.printf(" The %1$s Inch is %2$s Meter" + "\r\n"  ,inch,inch_to_meter);
+        System.out.printf(" The %1$s Inch is %2$s Centimeter" + "\r\n"  ,inch,inch_to_centimeter);
+
+
+    }
+    public static void main ( String [] args){
+        read();
+        Calculate();
+        write();
+    }
 
 }
